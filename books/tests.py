606027bd73c4060ab6467a03ea415017f44dd153
Saifurrahmanemon <saifemon789@gmail.com>
2021-10-21 12:59:57 +0100

    working with auth and athorization

--- books/tests.py
+++ books/tests.py
@@ -1,47 +1,60 @@
-
-from django.test import Client, TestCase
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Permission  # new
+from django.test import TestCase
 from django.urls import reverse
 from .models import Book, Review
-from django.contrib.auth import get_user_model
 
 
-class BookTest(TestCase):
+class BookTests(TestCase):
 
     def setUp(self):
         self.user = get_user_model().objects.create_user(
-            username="testsaif",
-            email="test@books.com",
-            password="test123"
+            username='testinguser',
+            email='testinguser@email.com',
+            password='passfortest'
         )
-
+        self.special_permission = Permission.objects.get(
+            codename='special_status')  # new
         self.book = Book.objects.create(
-            title="good boy",
-            author="saifur",
-            price='32.323',
+            title='I am mister test',
+            author='saifur',
+            price='32.99',
         )
-
         self.review = Review.objects.create(
             book=self.book,
             author=self.user,
-            review="test review"
+            review='An excellent review',
         )
 
-    def test_booklisting(self):
-        self.assertEqual(f'{self.book.title}', 'good boy')
+    def test_book_listing(self):
+        self.assertEqual(f'{self.book.title}', 'I am mister test')
         self.assertEqual(f'{self.book.author}', 'saifur')
-        self.assertEqual(f'{self.book.price}', '32.323')
+        self.assertEqual(f'{self.book.price}', '32.99')
 
-    def test_book_listview(self):
+    def test_book_list_view_for_logged_in_user(self):  # new
+        self.client.login(email='testinguser@email.com', password='passfortest')
         response = self.client.get(reverse('book_list'))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'good boy')
+        self.assertContains(response, 'I am mister test')
         self.assertTemplateUsed(response, 'book/book_list.html')
 
-    def test_book_detailview(self):
+    def test_book_list_view_for_logged_out_user(self):  # new
+        self.client.logout()
+        response = self.client.get(reverse('book_list'))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(
+            response, '%s?next=/books/' % (reverse('account_login')))
+        response = self.client.get(
+            '%s?next=/books/' % (reverse('account_login')))
+        self.assertContains(response, 'Log In')
+
+    def test_book_detail_view_with_permissions(self):  # new
+        self.client.login(email='testinguser@email.com', password='passfortest')
+        self.user.user_permissions.add(self.special_permission)
         response = self.client.get(self.book.get_absolute_url())
-        no_response = self.client.get('/books/2897')
+        no_response = self.client.get('/book/12345/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(no_response.status_code, 404)
-        self.assertContains(response, 'good boy')
-        self.assertContains(response, "test review")
+        self.assertContains(response, 'I am mister test')
+        self.assertContains(response, 'An excellent review')
         self.assertTemplateUsed(response, 'book/book_detail.html')
--- books/views.py
+++ books/views.py
@@ -1,14 +1,17 @@
 from django.views.generic import ListView, DetailView
 from .models import Book
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
 
-class BookListView(ListView):
+class BookListView(LoginRequiredMixin, ListView):
     model = Book
     template_name = 'book/book_list.html'
     context_object_name = 'book_list'
 
 
-class BookDetailView(DetailView):
+class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     model = Book
     template_name = 'book/book_detail.html'
     context_object_name = 'book'
+    login_url = 'account_login'
+    permission_required = 'book.special_status'

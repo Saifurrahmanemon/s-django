d801e4dbaad91dc98090f20c4d215ebcb8fdbc2c
Saifurrahmanemon <saifemon789@gmail.com>
2021-11-09 14:49:24 +0000

    ....

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get("DEBUG", default=False)
 
 ALLOWED_HOSTS = ['d-bookstoree.herokuapp.com', 'localhost', '127.0.0.1']
 
